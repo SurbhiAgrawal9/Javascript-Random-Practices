e782df5e297e38b2190d9aa10426e5c2d602538d
Surbhi Agrawal <111246726+SurbhiAgrawal9@users.noreply.github.com>
2023-05-25 23:06:19 +0530

    Create Day-2Math-Property.js

---
+++ Day-2Math-Property.js
@@ -0,0 +1,34 @@
+//Math Function   0.0 to 1.0 
+let r= Math.random();
+console.log(r)
+
+
+//fetch the two digits random number
+let r= Math.random();
+let res = r* 100     //adding zero according to 2,3, ...up to n digit
+console.log(res)
+
+
+//remove dot value 
+console.log(parseInt(res))   
+
+
+// find maximumm value
+let a = Math.max(2,3,77,5,9,2,55,22)
+console.log(a)
+
+//find minmum value
+let a = Math.min(2,3,77,5,9,2,55,22)
+console.log(a)
+
+//  upper level round of   
+let a =5.2
+console.log(Math.ceil(a))   //6
+
+// lower level round of 
+let a =5.3
+console.log(Math.floor(a))   //5
+
+let a =5.3
+console.log(Math.round(a))  //5
+
