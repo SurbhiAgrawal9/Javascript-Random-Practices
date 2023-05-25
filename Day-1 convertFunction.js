db12c01467d1207e52afb50f83fe3912fd40a445
Surbhi Agrawal <111246726+SurbhiAgrawal9@users.noreply.github.com>
2023-05-25 20:01:31 +0530

    Create Day-1 convertFunction.js

---
+++ Day-1 convertFunction.js
@@ -0,0 +1,24 @@
+Types of 
+//convert into integer
+var a = 10;
+var b = "20";
+var c = a + parseInt(b);
+console.log(c)  //30
+console.log(typeof c);
+
+
+//convert into float
+var a = 10;
+var b = "20.35";
+var c = a + parseFloat(b);
+console.log(c) //30.35
+console.log(typeof c);
+
+
+//convert into Number  for remove confusion
+var a = 10;
+var b = "20.35";
+var c = a + Number(b);
+console.log(c)  //30.35
+console.log(typeof c);
+
